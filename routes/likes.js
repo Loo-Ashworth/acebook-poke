dd3bd7391d72a035f43534e5ddbbb8adff9f1a41
Terence Roach <terryroach180@gmail.com>
2023-06-20 11:08:34 +0100

    updated tests including friends, posts and comments

--- routes/likes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const LikesController = require("../controllers/likes");
-
-router.post("/", LikesController.Create);
-
-module.exports = router;
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { parser } = require("../cloudinary");
-const UsersController = require("../controllers/users");
-
-router.get("/new", UsersController.New);
-router.post("/", parser.single("image"), UsersController.Create);
-
-module.exports = router;
