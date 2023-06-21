c92c120a84aa14d46cd614be879ba00dc219318a
Joan kalanzi <72227085+JoanKalanzi@users.noreply.github.com>
2023-06-21 10:09:17 +0100

    comment edit and delete added

--- controllers/comments.js
+++ controllers/comments.js
@@ -16,6 +16,37 @@ const CommentsController = {
       res.redirect("/posts");
     }
   },
+  edit: async (req, res) => {
+    try {
+      const comment = await Comment.findById(req.params.id);
+      res.render("edit-comment", { comment });
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/posts");
+    }
+  },
+
+  update: async (req, res) => {
+    try {
+      await Comment.findByIdAndUpdate(req.params.id, {
+        content: req.body.updatedContent,
+      });
+      res.redirect("/posts");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/posts");
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      await Comment.findByIdAndDelete(req.params.id);
+      res.redirect("/posts");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/posts");
+    }
+  },
 };
 
 module.exports = CommentsController;
--- routes/comments.js
+++ routes/comments.js
@@ -4,4 +4,12 @@ const CommentsController = require("../controllers/comments");
 
 router.post("/", CommentsController.Create);
 
+router.get("/:id/edit", CommentsController.edit);
+
+
+router.put("/:id", CommentsController.update);
+
+
+router.delete("/:id", CommentsController.delete);
+
 module.exports = router;
